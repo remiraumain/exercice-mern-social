6e0bb4bf94daa2b54936a3923ada00334927ea70
Rémi Raumain <remiraumain@Mac-mini-de-Remi.local>
2022-12-20 11:41:33 +0100

    Added redux logic

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://remiraumain:test123@cluster0.xrhwhqp.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
